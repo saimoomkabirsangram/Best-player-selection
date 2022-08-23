4b5375d0f43a4640b9446fffdb976863c90ca6c3
saimoomkabirsangram <saimoomkabirsangram@gmail.com>
2022-08-23 18:34:43 +0600

    total amount calculate

--- js/calculatePlayerAmount.js
+++ js/calculatePlayerAmount.js
@@ -1,55 +1,4 @@
-// // budget calculate 
-// document.getElementById('budget-calculate').addEventListener('click', function(){
-//     let perPlayerCostValue = document.getElementById('per-people-cost')
-//     let perPlayerString = perPlayerCostValue.value ;
-//     let perPlayerCost = parseFloat(perPlayerString);
-
-//     // selected player value 
-//     const playerTableBodyElement = document.getElementById('selected-heros');
-//     let playerValueString = playerTableBodyElement.innerText;
-//     let playerLenth = parseFloat(playerValueString);
-//     playerLenth.innerText = perPlayerCost ;
-
-//     // calculate per player cost
-//     const totalPlayerCost = playerLenth * perPlayerCost ;
-
-//     // player expence 
-//     let playerExpence = document.getElementById('player-expence');
-//     let playerExpenceString = playerExpence.innerText;
-//     let totalPlayerExpence = parseFloat(playerExpenceString);
-//     playerExpence.innerText = totalPlayerCost;
-
-//     // manager cost 
-//     let managerCost = document.getElementById('manager-cost');
-//     let managerCostString = managerCost.value;
-//     let managerCostValue = parseFloat(managerCostString);
-
-//     // coach cost 
-//     let coachCost = document.getElementById('coach-cost');
-//     let coachCostString = coachCost.value;
-//     let coachCostValue = parseFloat(coachCostString);
-
-//     // total cost 
-//     totalPlayerExpence + managerCostValue + coachCostValue;
-// })
-// document.getElementById('total-calculate').addEventListener('click', function(){
-//     // manager cost 
-//     let managerCost = document.getElementById('manager-cost');
-//     let managerCostString = managerCost.value;
-//     let managerCostValue = parseFloat(managerCostString);
-
-//     // coach cost 
-//     let coachCost = document.getElementById('coach-cost');
-//     let coachCostString = coachCost.value;
-//     let coachCostValue = parseFloat(coachCostString);
-
-//     let playerExpenceValue = document.getElementById('player-expence');
-//     // total cost 
-//     let final = managerCostValue + coachCostValue + playerExpenceValue;
-//     console.log(final);
-
-// })
-
+// Common Function 
 function getInputValueById(inputId){
     let costValue = document.getElementById(inputId);
     let costValueString = costValue.value ;
@@ -73,7 +22,34 @@ document.getElementById('budget-calculate').addEventListener('click', function()
     
 
     let playerExpence = document.getElementById('player-expence');
-    let playerExpencestr = parseFloat(playerExpence);
+    let playerExpencestr = playerExpence.innerText
+    let playerExpenceNumber = parseFloat(playerExpencestr);
     playerExpence.innerText = totalCost;
+})
+
+
+// button select 
+document.getElementById('total-calculate').addEventListener('click', function(){
+    let managerCost = getInputValueById('manager-cost')
+    let coachCost = getInputValueById('coach-cost')
+
+    let totalMangerCoachCost = managerCost + coachCost ;
+
+    let playerExpence = document.getElementById('player-expence');
+    let playerExpencestr = playerExpence.innerText
+    let playerExpenceNumber = parseFloat(playerExpencestr);
+    // playerExpence.innerText = totalCost;
+    console.log(playerExpenceNumber);
+
+    playerExpencestr.innerText = totalMangerCoachCost;
+
+    let finalCostValue = totalMangerCoachCost + playerExpenceNumber;
+
+
+
+    let getTotalValue = document.getElementById('total-cost')
+    let totalCoststr = getTotalValue.innerText;
+    let finalCost = parseFloat(totalCoststr);
+    getTotalValue.innerText = finalCostValue;
 
 })
--- js/playerSelction.js
+++ js/playerSelction.js
@@ -31,9 +31,9 @@ function playerNameDsiplay(){
     const playerTableBodyElement = document.getElementById('selected-heros');
     playerTableBodyElement.innerText = playerArr.length ;
 }
-// button select 
+// button select    
 function buttonSelect(element) {
-    const btnDisable = element.setAttribute('disabled', '')
+    const btnDisable = element.setAttribute("disabled", '')
     let getplayerName = element.parentNode.children[0].innerText;
 
     const playerObj = {
@@ -42,61 +42,3 @@ function buttonSelect(element) {
     playerArr.push(playerObj);
     playerNameDsiplay(playerArr);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // selected hero 
-// let playerArr = [];
-// function playerNameDsiplay(){
-//     const playerTableBodyElement = document.getElementById('selected-heros');
-//     playerTableBodyElement.innerText = playerArr.length ;
-//     const playerTableBody = document.getElementById('player-table-body');
-//     playerTableBody.textContent = '';
-
-//     const tr = document.createElement('tr');
-//     tr.innerHTML = `
-//     <td>Player</td>
-//     <td>${playerArr.length}</td>`
-
-//     playerTableBody.appendChild(tr)
-
-    
-//     for(let i = 0; i < playerArr.length; i++){
-//         let playerName = playerArr[i].getplayerName;
-
-//         const tr = document.createElement('tr')
-//         tr.innerHTML = `
-//         <tr>
-//             <th>${i+1}</th>
-//             <td>${playerName}</td>
-//         </tr>`
-
-//         playerTableBody.appendChild(tr);
-       
-//     }
-// }
-// // button select 
-// function buttonSelect(element) {
-//     let getplayerName = element.parentNode.children[0].innerText;
-
-//     const playerObj = {
-//         getplayerName: getplayerName
-//     }
-//     playerArr.push(playerObj);
-//     playerNameDsiplay(playerArr);
-// }
